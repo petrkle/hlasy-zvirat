2171d3e66597b61e5abc44df68fbd597a4befa18
Petr <petr@kle.cz>
2023-09-18 22:28:48 +0200

    Formátování java

--- app/src/main/java/cz/kle/hlasyzvirat/HlasyZvirat.java
+++ app/src/main/java/cz/kle/hlasyzvirat/HlasyZvirat.java
@@ -3,13 +3,13 @@ package cz.kle.hlasyzvirat;
 import android.os.Bundle;
 import org.apache.cordova.*;
 
-public class HlasyZvirat extends CordovaActivity 
+public class HlasyZvirat extends CordovaActivity
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-				Bundle extras = getIntent().getExtras();
+        Bundle extras = getIntent().getExtras();
         if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
             moveTaskToBack(true);
         }
